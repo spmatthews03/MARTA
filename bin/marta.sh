786baeed1fe609fd4aadc505625133db7a812b17
Martin Smith <theMinCAMP@outlook.com>
2018-06-25 21:39:01 -0700

    initial

---
+++ bin/marta.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -cp "./postgresql-42.2.1.jar:./mts.jar" edu.gatech.Main < marta.txt > marta_output.txt
---
+++ bin/simple.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -jar ./mts.jar < simple_test.txt > simple_output.txt
