e98004b367c49fcca81811d6c7397297b53d81fa
Martin Smith <theMinCAMP@outlook.com>
2018-07-08 17:34:22 -1000

    FileMain

---
+++ workspace/MTS/src/group_a7_8/FileMain.java
@@ -0,0 +1,56 @@
+package group_a7_8;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import edu.gatech.SimDriver;
+
+public class FileMain {
+	private static final String INPUT_FILE_TOKEN="-input:";
+
+	public static void main(String[] args) throws IOException {
+		  System.out.println("Mass Transit Simulation System using file source Starting...");
+		  String inputScriptFileName = null;
+		  
+		  SimDriver commandInterpreter = new SimDriver();
+		  
+		  for(String arg:args) {
+			  System.out.printf("arg %s\n",arg);
+			  if(arg.startsWith(INPUT_FILE_TOKEN)) {
+				  inputScriptFileName = arg.substring(INPUT_FILE_TOKEN.length()).trim();
+			  }
+		  }
+		  
+		  if(inputScriptFileName==null) {
+			  printUsage();
+			  System.exit(0);
+		  }
+		  File inFile = new File(inputScriptFileName);
+		  if(!inFile.exists() || !inFile.isFile()) {
+			  System.out.printf("ERROR: %s is an invalid input file\n",inputScriptFileName);
+			  printUsage();
+			  System.exit(0);
+		  }
+
+		  System.out.printf("\tinput:%s\n",inputScriptFileName);  
+
+	      BufferedReader br = new BufferedReader(new FileReader(inFile));
+	      String line = br.readLine();
+	      while(line!=null) {
+	    	  System.out.printf("%s\n",line);
+	    	  commandInterpreter.processCommand(line);
+	    	  line = br.readLine();
+	      }	   
+	      br.close();
+	}
+
+	public static  void printUsage() {
+		String message = "Arguments:\n"
+				+ "\t-input:<input file> - relative or full path of input file - REQUIRED\n"
+				+ "\nExample:\n"
+				+ "\t1)\tjava -cp:./mts.jar group_a7_8.FileMain -input:./simple_test.txt\n";
+		System.out.printf("%s\n", message);
+	}
+}
