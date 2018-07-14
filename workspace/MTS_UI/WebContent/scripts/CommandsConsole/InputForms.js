571948f90d76649245096428ed475061c67a3434
sean <smatthews39@gatech.edu>
2018-07-14 07:07:25 -0700

    additional forms

--- workspace/MTS_UI/WebContent/scripts/CommandsConsole/InputForms.js
+++ workspace/MTS_UI/WebContent/scripts/CommandsConsole/InputForms.js
@@ -57,6 +57,69 @@ var addBusPathDelayFormDirective = function(){
 templateUrl:'scripts/CommandsConsole/BusPathDelay.html'
 }
 };
+var setBusSpeedLimitFormDirective = function(){
+	return{
+		restrict:'E',
+		scope:{},
+		controller: 'inputFormController',
+		replace: true,
+		templateUrl:'scripts/CommandsConsole/BusSpeedLimit.html'
+	}
+};
+var addTrainStationFormDirective = function(){
+	return{
+		restrict:'E',
+		scope:{},
+		controller: 'inputFormController',
+		replace: true,
+		templateUrl:'scripts/CommandsConsole/AddTrainStop.html'
+	}
+};
+var addRailLineFormDirective = function(){
+	return{
+		restrict:'E',
+		scope:{},
+		controller: 'inputFormController',
+		replace: true,
+		templateUrl:'scripts/CommandsConsole/AddTrainRoute.html'
+	}
+};
+var extendRailLineFormDirective = function(){
+	return{
+		restrict:'E',
+		scope:{},
+		controller: 'inputFormController',
+		replace: true,
+		templateUrl:'scripts/CommandsConsole/ExtendTrainRoute.html'
+	}
+};
+var addTrainFormDirective = function(){
+	return{
+		restrict:'E',
+		scope:{},
+		controller: 'inputFormController',
+		replace: true,
+		templateUrl:'scripts/CommandsConsole/AddTrain.html'
+	}
+};
+var addTrainPathDelayFormDirective = function(){
+	return{
+		restrict:'E',
+		scope:{},
+		controller: 'inputFormController',
+		replace: true,
+		templateUrl:'scripts/CommandsConsole/TrainPathDelay.html'
+	}
+};
+var setTrainSpeedLimitFormDirective = function(){
+	return{
+		restrict:'E',
+		scope:{},
+		controller: 'inputFormController',
+		replace: true,
+		templateUrl:'scripts/CommandsConsole/TrainSpeedLimit.html'
+	}
+};
 //controllers
   var inputFormController = function($scope, $log, mtsService){
 	  $log.info('inputFormController');
@@ -91,6 +154,27 @@ templateUrl:'scripts/CommandsConsole/BusPathDelay.html'
 		  case 'pathDelay':
 			  command = 'path_delay,'+$scope.item.time+','+$scope.item.duration+','+$scope.item.originStopId+','+$scope.item.destinationStopId+','+$scope.item.delayFactor;
 			  break;
+		  case 'setBusSpeedLimit':
+			  command = 'add_train,'+$scope.item.time+','+$scope.item.duration+','+$scope.item.originStopId+','+$scope.item.destinationStopId+','+$scope.item.delayFactor;
+			  break;
+		  case 'addTrainStation':
+			  command = 'add_station,'+$scope.item.time+','+$scope.item.duration+','+$scope.item.originStopId+','+$scope.item.destinationStopId+','+$scope.item.delayFactor;
+			  break;
+		  case 'addRailLine':
+			  command = 'path_delay,'+$scope.item.time+','+$scope.item.duration+','+$scope.item.originStopId+','+$scope.item.destinationStopId+','+$scope.item.delayFactor;
+			  break;
+		  case 'extendRailLine':
+			  command = 'path_delay,'+$scope.item.time+','+$scope.item.duration+','+$scope.item.originStopId+','+$scope.item.destinationStopId+','+$scope.item.delayFactor;
+			  break;
+		  case 'addTrainPathDelay':
+			  command = 'path_delay,'+$scope.item.time+','+$scope.item.duration+','+$scope.item.originStopId+','+$scope.item.destinationStopId+','+$scope.item.delayFactor;
+			  break;
+		  case 'addBusPathDelay':
+			  command = 'path_delay,'+$scope.item.time+','+$scope.item.duration+','+$scope.item.originStopId+','+$scope.item.destinationStopId+','+$scope.item.delayFactor;
+			  break;
+		  case 'setTrainSpeedLimit':
+			  command = 'path_delay,'+$scope.item.time+','+$scope.item.duration+','+$scope.item.originStopId+','+$scope.item.destinationStopId+','+$scope.item.delayFactor;
+			  break;
 		  }
 		  if(command && command.length>0){
 			  $log.info('command: '+command);
@@ -112,5 +196,12 @@ templateUrl:'scripts/CommandsConsole/BusPathDelay.html'
   .directive('addBusForm',[addBusFormDirective])
   .directive('addEventForm',[addEventFormDirective])
   .directive('addBusPathDelayForm',[addBusPathDelayFormDirective])
+  .directive('setBusSpeedLimitForm',[setBusSpeedLimitFormDirective])
+  .directive('addTrainStationForm',[addTrainStationFormDirective])
+  .directive('addRailLineForm',[addRailLineFormDirective])
+  .directive('extendRailLineForm',[extendRailLineFormDirective])
+  .directive('addTrainForm',[addTrainFormDirective])
+  .directive('addTrainPathDelayForm',[addTrainPathDelayFormDirective])
+  .directive('setTrainSpeedLimitForm',[setTrainSpeedLimitFormDirective])
   .controller('inputFormController',['$scope', '$log','MTSService', inputFormController]);
 }());
