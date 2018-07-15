557e05473b69c4c97fae64107b7c5a25a0e1cc7a
Sebastian Sanchez <ssanchez44@gatech.edu>
2018-07-15 18:17:48 -0400

    Add JSON query to Depot and RailStation classes.

--- workspace/MTS/src/edu/gatech/RailStation.java
+++ workspace/MTS/src/edu/gatech/RailStation.java
@@ -6,4 +6,26 @@ public class RailStation extends ExchangePoint {
 		super(uniqueValue, inputName, inputXCoord, inputYCoord,"RailStation");
 	}
 
+    public String toJSON() {
+    	StringBuilder sb = new StringBuilder();
+    	sb.append('{');
+       	sb.append("\"type\":\"RailStation\",");
+    	sb.append("\"ID\":");
+    	sb.append(this.get_uniqueID());
+    	sb.append(',');
+    	sb.append("\"name\":\"");
+    	sb.append(this.getFacilityName());
+    	sb.append('\"');
+    	sb.append(',');
+    	sb.append("\"xCoord\":");
+    	sb.append(this.getLocation().getX());
+    	sb.append(',');
+    	sb.append("\"yCoord\":");
+    	sb.append(this.getLocation().getY());
+    	sb.append(',');
+    	sb.append("\"waiting\":");
+    	sb.append(this.get_riders());
+    	sb.append('}');
+    	return sb.toString();
+    }
 }
