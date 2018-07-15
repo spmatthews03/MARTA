ada46756bc5e6fb196b6f82c5cae7f5846dd70d2
Martin Smith <theMinCAMP@outlook.com>
2018-07-15 12:35:52 -0700

    fixed route serialization for name attribute

--- workspace/MTS/src/edu/gatech/VehicleRoute.java
+++ workspace/MTS/src/edu/gatech/VehicleRoute.java
@@ -85,9 +85,10 @@ public abstract class VehicleRoute {
  	   sb.append("\"number\":");
  	   sb.append(this.routeNumber);
  	   sb.append(',');
- 	   sb.append("\"name\":");
+ 	   sb.append("\"name\":\"");
  	   sb.append(this.getName());
- 	   if(this.exchangePointsOnRoute!=null && this.exchangePointsOnRoute.size()>0) {
+ 	   sb.append('\"');
+	   if(this.exchangePointsOnRoute!=null && this.exchangePointsOnRoute.size()>0) {
  	      	sb.append(',');
  	      	sb.append("\"" + exchangePointType + "\":[");    
  	      	for(int key = 0; key<this.exchangePointsOnRoute.size();key++) {
