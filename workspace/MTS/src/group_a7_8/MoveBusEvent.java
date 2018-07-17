6df60f9b72e87da2d4b0383d86efdfb8134f8cef
sean <smatthews39@gatech.edu>
2018-07-16 20:36:56 -0700

    modification to movebus, if/elseif/else regarding fuel and out of service

--- workspace/MTS/src/group_a7_8/MoveBusEvent.java
+++ workspace/MTS/src/group_a7_8/MoveBusEvent.java
@@ -41,6 +41,7 @@ public class MoveBusEvent extends SimEvent{
         Bus activeBus = getBus();
         System.out.println(" the bus being observed is: " + Integer.toString(activeBus.getID()));
 
+
         // identify the current stop
         BusRoute activeRoute = system.getBusRoute(activeBus.getRouteID());
         System.out.println(" the bus is driving on route: " + Integer.toString(activeRoute.getID()));
@@ -50,12 +51,6 @@ public class MoveBusEvent extends SimEvent{
         BusStop activeStop = system.getBusStop(activeStopID);
         System.out.println(" the bus is currently at stop: " + Integer.toString(activeStop.get_uniqueID()) + " - " + activeStop.getFacilityName());
 
-        // drop off and pickup new passengers at current stop
-        int currentPassengers = activeBus.getPassengers();
-        int passengerDifferential = activeStop.exchangeRiders(getRank(), currentPassengers, activeBus.getCapacity());
-        System.out.println(" passengers pre-stop: " + Integer.toString(currentPassengers) + " post-stop: " + (currentPassengers + passengerDifferential));
-        activeBus.adjustPassengers(passengerDifferential);
-
         // determine next stop
         int nextLocation = activeRoute.getNextLocation(activeLocation);
         int nextStopID = activeRoute.getStopID(nextLocation);
@@ -64,27 +59,73 @@ public class MoveBusEvent extends SimEvent{
 
         // find distance to stop to determine next event time
         Double distanceToNextStopThenDepot = activeStop.findDistance(nextStop) + nextStop.findDistance(system.getDepot());
-        int travelTime = 1 + (distanceToNextStopThenDepot.intValue() * 60 / activeBus.getSpeed());
 
-        // check if bus has enough fuel to go to next stop then depot, if not, go directly to depot
-        if(activeBus.hasEnoughFuel(distanceToNextStopThenDepot)){
+        if(!activeBus.getOutOfService() && activeBus.hasEnoughFuel(distanceToNextStopThenDepot)) {
+
+            // check if bus has enough fuel to go to next stop then depot, if not, go directly to depot
+            int travelTime = 1 + (distanceToNextStopThenDepot.intValue() * 60 / activeBus.getSpeed());
+
+            // Create a fuel report to next stop
             FuelConsumption report = new FuelConsumption(activeBus, new PathKey(activeStop, nextStop),
                     travelTime, activeStop.findDistance(nextStop));
             system.getFuelConsumptionList(activeBus).add(report);
+
+            // drop off and pickup new passengers at current stop
+            int currentPassengers = activeBus.getPassengers();
+            int passengerDifferential = activeStop.exchangeRiders(getRank(), currentPassengers, activeBus.getCapacity());
+            System.out.println(" passengers pre-stop: " + Integer.toString(currentPassengers) + " post-stop: " + (currentPassengers + passengerDifferential));
+            activeBus.adjustPassengers(passengerDifferential);
+
+            // conversion is used to translate time calculation from hours to minutes
+            activeBus.setLocation(nextLocation);
+
+            // generate next event for this bus
+            eventQueue.add(new MoveBusEvent(system, eventID, getRank() + travelTime,bus));
+        }
+        else if(activeBus.getOutOfService()){
+            // calculate distance to depot
+            Double distanceToDepot = activeStop.findDistance(system.getDepot());
+            int travelTime = 1 + (distanceToDepot.intValue() * 60 / activeBus.getSpeed());
+
+            // create fuel report for traveling to depot
+            FuelConsumption report = new FuelConsumption(activeBus, new PathKey(activeStop, system.getDepot()),
+                    travelTime, distanceToDepot);
+            system.getFuelConsumptionList(activeBus).add(report);
+
+            // drop all passengers before moving to depot
+            int dropAllPassengers = activeBus.getPassengers();
+            activeBus.adjustPassengers(-(dropAllPassengers));
+
+            // set location to first stop and next location to 2nd stop
+            activeBus.setLocation(activeRoute.getStopID(0));
+            activeRoute.getNextLocation(1);
         }
         else{
-            FuelConsumption report = new FuelConsumption(activeBus, new PathKey(activeStop, nextStop),
-                    travelTime, activeStop.findDistance(system.getDepot()));
+            // calculate distance to depot
+            Double distanceToDepot = activeStop.findDistance(system.getDepot());
+            int travelTime = 1 + (distanceToDepot.intValue() * 60 / activeBus.getSpeed());
+
+            // create fuel report for traveling to depot
+            FuelConsumption report = new FuelConsumption(activeBus, new PathKey(activeStop, system.getDepot()),
+                    travelTime, distanceToDepot);
             system.getFuelConsumptionList(activeBus).add(report);
+
+            // drop all passengers before moving to depot
+            int dropAllPassengers = activeBus.getPassengers();
+            activeBus.adjustPassengers(-(dropAllPassengers));
+
+            // set location to next location
+            activeBus.setLocation(nextLocation);
+
+            // TODO: create fuel report for traveling FROM Depot TO next stop
+
+            // create event to set vehicle out of service to refuel
+            eventQueue.add(new VehicleOutOfServiceEvent(system,eventID,getRank() + travelTime,bus));
+
+            // create event to resume service after refueling
+            eventQueue.add(new VehicleResumeServiceEvent(system,eventID,getRank()+travelTime,bus));
+
         }
-
-        // conversion is used to translate time calculation from hours to minutes
-        activeBus.setLocation(nextLocation);
-
-
-        // generate next event for this bus
-        eventQueue.add(new MoveBusEvent(system, eventID, getRank() + travelTime,bus));               
-		
 	}
-
 }
+
