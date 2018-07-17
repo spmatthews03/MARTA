017623d8670684bc121588818a3b235904d86e2b
Martin Smith <theMinCAMP@outlook.com>
2018-07-17 09:25:26 -0700

    rudimentary fuel consumption report

--- bin/WebContent/scripts/SimExecution/FuelConsumption.js
+++ bin/WebContent/scripts/SimExecution/FuelConsumption.js
@@ -15,6 +15,28 @@
   var fuelConsumptionController = function($scope, $log, mtsService){
 		$log.info('fuelConsumptionController');
 		$scope.reports = mtsService.state.reports;
+		$scope.getTotal = function(vehicle){
+			var totalAmount=0;
+			
+				$log.info('fuel report for ');
+				$log.info(vehicle);
+				for(var i=0;i<mtsService.state.reports.length;i++){
+					var vehicleReport = mtsService.state.reports[i];
+					$log.info(vehicleReport);
+					if(vehicleReport.vehicle.type==vehicle.type && vehicleReport.vehicle.ID == vehicle.ID){
+						for(var j=0;j<vehicleReport.reports.length;j++){
+							var report = vehicleReport.reports[j];
+							$log.info(vehicleReport);
+							totalAmount = totalAmount+report.amount;
+							$log.info('totalAmount: '+totalAmount);
+						}
+					}
+				}
+			
+
+			return totalAmount;
+		};
+		
   };
  
   angular.module('SimExec')
